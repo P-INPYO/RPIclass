0303ccdc45b108bf82fe170419176d510241b6d4
P-INPYO <inpyo0822@naver.com>
2023-01-03 15:38:30 +0900

    pip

--- PIP.py
+++ PIP.py
@@ -1,15 +1,7 @@
-import datetime
-
-import Adafruit_DHT as dht
-
-#온습도센서 모듈에 따라 DHT11, DHT22 변경해야 함
-
-#DHT센서의 핀번호 보통 4를 입력
-
-h,t = dht.read_retry(dht.DHT11,4)
-
-print('Hello')
-
-print (h)
-
-print (t)
+a = int(input("num\n"))
+if (a>10):
+    print("10보다큼")
+elif(a==10):
+    print("10이다.")
+else:
+    print("10보다작음")
