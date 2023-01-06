3180963d53f799a02fab9f2f903d99e14d1ddf58
P-INPYO <inpyo0822@naver.com>
2023-01-06 12:40:03 +0900

    v2

--- Ultra.py
+++ Ultra.py
@@ -15,13 +15,6 @@ mylcd = I2C_driver.lcd()
 def FND(pin):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(PIN,GPIO.OUT)
-    if sel == 1:
-        mylcd.lcd_clear()
-        mylcd.lcd_display_string(f"{sel}:FND", 1)
-        print('enter 1~9')
-        f = int(input())
-        mylcd.lcd_display_string(f"{f}", 2)
-        pin = DISPLAY[f] 
     for i in range(7):
         if(pin&NUM[i] == NUM[i]):
 
@@ -33,82 +26,87 @@ def FND(pin):
 
             
 def ultra():
-    GPIO.setmode(GPIO.BCM)  
-    GPIO.setup(PinTrig, GPIO.OUT)           
-    GPIO.setup(PinEcho, GPIO.IN)
+    motor(1)
     mylcd.lcd_clear()
+    mylcd.lcd_display_string(f"DOOR close", 2)
     mylcd.lcd_display_string(f"{sel}:ultra", 1)
     while 1:
-            startTime=0
-            stopTime=0
-            GPIO.output(PinTrig, False)    
-            time.sleep(2)
-            GPIO.output(PinTrig, True)          
-            time.sleep(0.00001)            
-            GPIO.output(PinTrig, False)         
+        GPIO.setmode(GPIO.BCM)  
+        GPIO.setup(PinTrig, GPIO.OUT)           
+        GPIO.setup(PinEcho, GPIO.IN)
+        startTime=0
+        stopTime=0
+        GPIO.output(PinTrig, False)    
+        time.sleep(2)
+        GPIO.output(PinTrig, True)          
+        time.sleep(0.00001)            
+        GPIO.output(PinTrig, False)         
 
-            while GPIO.input(PinEcho) == 0:   
-                startTime = time.time()
-            while GPIO.input(PinEcho) == 1:   
-                stopTime = time.time()
+        while GPIO.input(PinEcho) == 0:   
+            startTime = time.time()
+        while GPIO.input(PinEcho) == 1:   
+            stopTime = time.time()
 
-            Time_interval= stopTime - startTime     
-            Distance = Time_interval * 17000
-            Distance = round(Distance, 2)
-            mylcd.lcd_display_string(f"{Distance}", 2)
-            print ('Distance => ', Distance, 'cm')
-            if int(Distance) >= 30:
-                pin = DISPLAY[3]       
-                FND(pin)
-            elif int(Distance) >= 20:
-                pin = DISPLAY[2]       
-                FND(pin)
-            elif int(Distance) >= 10:
-                pin = DISPLAY[1]       
-                FND(pin)
-            elif int(Distance) < 10:
-                mylcd.lcd_display_string("Door close", 2)
-                break
+        Time_interval= stopTime - startTime     
+        Distance = Time_interval * 17000
+        Distance = round(Distance, 2)
+        mylcd.lcd_display_string(f"{Distance}", 2)
+        print ('Distance => ', Distance, 'cm')
+        if int(Distance) >= 30:
+            pin = DISPLAY[3]       
+            FND(pin)
+        elif int(Distance) >= 20:
+            pin = DISPLAY[2]       
+            FND(pin)
+        elif int(Distance) >= 10:
+            pin = DISPLAY[1]       
+            FND(pin)
+        elif int(Distance) < 10:
+            mylcd.lcd_display_string("Door OPEN", 2)
+            motor(180)
+            break
 
-def motor():
+def motor(a):
     
     GPIO.setmode (GPIO.BCM)           
     GPIO.setup(12, GPIO.OUT) 
     Servo=GPIO.PWM(12, 40)
-    mylcd.lcd_clear()
-    mylcd.lcd_display_string(f"{sel}:servo", 1)
-    print('enter 0~180')
-    s = int(input())
-    mylcd.lcd_display_string(f"{s}", 2)
-    a = int(s)
     if a <= 18:
         duty_ratio = 1
     elif a == 180:
         duty_ratio = 9
     else:
-        duty_ratio = int(a/18)
+        duty_ratio = float(a/18)
     
     Servo.start(0)
     print('Wating for 1 sec') 
-    time.sleep(1) 
     
     Servo.ChangeDutyCycle(duty_ratio)
     time.sleep(0.5)
         
     
     Servo.stop()
-    GPIO.cleanup()
-    print('Everythings cleanup')
     
 
 while 1:
     print('1 : FND \n2 : servo \n3 : Ultra')
     sel = int(input())
     
-    if sel == 1:      
-        FND()
+    if sel == 1:
+        mylcd.lcd_clear()
+        mylcd.lcd_display_string(f"{sel}:FND", 1)
+        print('enter 1~9')
+        f = int(input())
+        mylcd.lcd_display_string(f"{f}", 2)
+        pin = DISPLAY[f] 
+        FND(pin)
     elif sel == 2:
-        motor()
+        mylcd.lcd_clear()
+        mylcd.lcd_display_string(f"{sel}:servo", 1)
+        print('enter 0~180')
+        a = int(input())
+        mylcd.lcd_display_string(f"{a}", 2)
+        motor(a)
     elif sel == 3:
         ultra()
 
